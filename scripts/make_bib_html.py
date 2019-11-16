4552a40ad383d2ec72346b8ba9789e23f201ddc0
Nathan Albin <albin@math.ksu.edu>
2019-11-16 10:19:44 -0600

    Updated bib script to remove comments from HTML file.

--- scripts/make_bib_html.py
+++ scripts/make_bib_html.py
@@ -9,11 +9,22 @@ bibdir  = '../references/'
 bibfile = 'References'
 htmldir = '../html/'
 
+# get rid of comments in bib file
+lines = []
+with open(bibdir + bibfile + '.bib', 'r') as f:
+	for line in f:
+		if line.upper().startswith('@COMMENT'):
+			continue
+		lines.append(line)
+with open(bibdir + bibfile + '.bib', 'w') as f:
+	f.writelines(lines)
+
 # get rid of unwanted keys
 args = ['bib2bib', 
 		'-s', '$key',
 		'-ob', bibdir + bibfile + '.bib',
 		'--remove', '__markedentry',
+		'--remove', 'keywords',
 		'--no-comment',
 		bibdir + bibfile + '.bib']
 subprocess.check_call(args)
